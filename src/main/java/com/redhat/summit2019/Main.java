ca6f52be6b715d56551794388558623bcf091217
Ingo Weiss <ingo@redhat.com>
2019-03-15 16:06:13 +0000

    Added barebones REST facilities

--- src/main/java/com/redhat/summit2019/Main.java
+++ src/main/java/com/redhat/summit2019/Main.java
@@ -2,12 +2,33 @@ package com.redhat.summit2019;
 
 import com.redhat.summit2019.generator.PersonGenerator;
 import com.redhat.summit2019.model.Person;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
+import java.net.URL;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
+    private static OkHttpClient http = new OkHttpClient();
+
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 15; i++) {
+        int quantity = 10;
+        if (args != null && args.length > 0) {
+            quantity = Integer.parseInt(args[0]);
+        }
+
+        String baseURI = "http://127.0.0.1:8080/greeting/";
+
+        if (args != null && args.length > 1) {
+            baseURI = args[1];
+        }
+
+        URL url;
+
+        for (int i = 0; i < quantity; i++) {
             int random = ThreadLocalRandom.current().nextInt(3);
             Person person;
             if (random == 0) {
@@ -17,8 +38,12 @@ public class Main {
             } else {
                 person = PersonGenerator.getPerson("m");
             }
-            System.out.println(person.toJSON());
-        }
+            url = new URL(baseURI + i);
+            RequestBody body = RequestBody.create(MediaType.get("application/json"), person.toJSON());
+            Request request = new Request.Builder().url(url).post(body).build();
+            Response response = http.newCall(request).execute();
 
+            System.out.println("POST: " + response.code() + " " + person.toJSON());
+        }
     }
 }
